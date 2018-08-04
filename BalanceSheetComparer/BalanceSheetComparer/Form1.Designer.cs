ac12119a9b1b88610477cc32350b71c96a428d87
Ashish <Ashish.Rathi@KLINGELNBERG.com>
2018-08-04 18:32:17 +0530

    Two labels, two text box and button added on to form

--- BalanceSheetComparer/BalanceSheetComparer/Form1.Designer.cs
+++ BalanceSheetComparer/BalanceSheetComparer/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.secondBalanceSheet = new System.Windows.Forms.TextBox();
+            this.secondBalanceSheeet = new System.Windows.Forms.TextBox();
+            this.generateReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Balance Sheet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Balance Sheet";
+            // 
+            // secondBalanceSheet
+            // 
+            this.secondBalanceSheet.Location = new System.Drawing.Point(242, 38);
+            this.secondBalanceSheet.Name = "secondBalanceSheet";
+            this.secondBalanceSheet.Size = new System.Drawing.Size(100, 20);
+            this.secondBalanceSheet.TabIndex = 2;
+            // 
+            // secondBalanceSheeet
+            // 
+            this.secondBalanceSheeet.Location = new System.Drawing.Point(242, 68);
+            this.secondBalanceSheeet.Name = "secondBalanceSheeet";
+            this.secondBalanceSheeet.Size = new System.Drawing.Size(100, 20);
+            this.secondBalanceSheeet.TabIndex = 3;
+            // 
+            // generateReport
+            // 
+            this.generateReport.Location = new System.Drawing.Point(228, 121);
+            this.generateReport.Name = "generateReport";
+            this.generateReport.Size = new System.Drawing.Size(114, 35);
+            this.generateReport.TabIndex = 4;
+            this.generateReport.Text = "Generate Report";
+            this.generateReport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.generateReport);
+            this.Controls.Add(this.secondBalanceSheeet);
+            this.Controls.Add(this.secondBalanceSheet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox secondBalanceSheet;
+        private System.Windows.Forms.TextBox secondBalanceSheeet;
+        private System.Windows.Forms.Button generateReport;
     }
 }
 
